4f28482ccd4d24025c363b4fa56de7af5977f7d9
anyuhang666 <44354050+anyuhang666@users.noreply.github.com>
2019-01-09 10:23:11 +0800

    1
    1

--- yun/src/main/java/com/dao/officeDao.java
+++
@@ -1,8 +0,0 @@
-package com.dao;
-
-import org.apache.ibatis.annotations.Param;
-
-
-public interface officeDao {
-    void addOffice(@Param("officeId") String officeId, @Param("officeMd5") String officeMd5) throws Exception;
-}
